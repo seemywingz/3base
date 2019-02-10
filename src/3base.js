62606ce9ab5914a270b1c73e160b647af361d87c
Kevin Jayne <kevin.jayne@icloud.com>
2019-02-10 15:05:01 -0500

    add rectengle

--- src/3base.js
+++ src/3base.js
@@ -1,19 +1,20 @@
 ('use-strict')
 
+export let AmbientLight = require("./AmbientLight").default;
+export let AMMO = require("ammo.js");
 export let Ball = require("./Ball").default;
-export let Camera = require("./Camera").default;
 export let Box = require("./Box").default;
+export let Camera = require("./Camera").default;
+export let DirectionalLight = require("./DirectionalLight").default;
 export let GLTFModel = require("./GLTFModel").default;
-export let OBJModel = require("./OBJModel").default;
 export let Ground = require("./Ground").default;
+export let HemisphereLight = require("./HemisphereLight").default;
 export let Manager = require("./Manager").default;
+export let OBJModel = require("./OBJModel").default;
+export let Rectangle = require("./Rectangle").default;
 export let Scene = require("./Scene").default;
 export let SceneObject = require("./SceneObject").default;
 export let Sky = require("./Sky").default;
-export let DirectionalLight = require("./DirectionalLight").default;
-export let AmbientLight = require("./AmbientLight").default;
-export let HemisphereLight = require("./HemisphereLight").default;
-export let AMMO = require("ammo.js");
 export let THREE = require("three");
 
 import UtilityFunctions from './UtilityFunctions';
