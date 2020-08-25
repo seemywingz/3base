09d7a91e2d5bf9b5fb2e5d80b1dcf4ee1fd78650
seemywingz <kevin.jayne@icloud.com>
2020-08-25 18:02:44 -0400

    add manager onerror

--- src/examples/index.js
+++ src/examples/index.js
@@ -4,7 +4,6 @@ import * as tb from '../3base';
 import Scene1 from './Scene1';
 import ExampleScene from './ExampleScene';
 
-// let manager = new tb.Manager({canvas: myCanvas});
 let manager = new tb.Manager({"onLoad": ()=>{
   document.getElementById( 'loading-screen' ).classList.add( 'fade-out' );
 }});
